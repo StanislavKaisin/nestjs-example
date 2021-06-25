296dda9768ae86df801786191605fe40988b5d4c
StanislavKaisin <41913995+StanislavKaisin@users.noreply.github.com>
2021-06-25 22:46:47 +0300

    used headers, req, res objects in nest

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,19 +1,27 @@
+import { Header, Req, Res } from '@nestjs/common';
 import {
   Body,
   Controller,
   Delete,
   Get,
+  HttpCode,
+  HttpStatus,
   Param,
   Post,
   Put,
+  Redirect,
 } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
+import { Request, Response } from 'express';
+
 @Controller('products')
 export class ProductsController {
   @Get()
-  getAll(): string {
+  // @Redirect('https://google.com', 301)
+  getAll(@Req() req: Request, @Res() res: Response): string {
+    res.status(201).end('hello');
     return 'getAll';
   }
 
@@ -24,12 +32,23 @@ export class ProductsController {
     return 'getOne ' +params.id
   }
 */
+
+  // express
+  /*
+app.use((req, res, next) => {
+  //
+  res.status(201).end('hello')
+})
+*/
+
   @Get(':id')
   getOne(@Param('id') id: string): string {
     return 'getOne ' + id;
   }
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
+  @Header('Cach-Control', 'none')
   create(@Body() createProductDto: CreateProductDto) {
     return `Title=${createProductDto.title}, Price=${createProductDto.price}`;
   }
