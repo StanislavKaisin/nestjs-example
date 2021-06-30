689a17b73e1b69f4cbe61baef029f4150600ea30
StanislavKaisin <41913995+StanislavKaisin@users.noreply.github.com>
2021-06-30 19:58:26 +0300

    i mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './products/products.controller';
+import { ProductsModule } from './products/products.module';
 import { ProductsService } from './products/products.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ProductsController],
-  providers: [AppService, ProductsService],
+  imports: [ProductsModule, MongooseModule.forRoot('mongodb://localhost/nest')],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
